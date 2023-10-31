1e7d07da4b13a46beb98d99456d25d184712d5e9
GabiStz <gabriele.steinmetz1901@gmail.com>
2023-10-30 22:44:02 -0300

    criando listar veiculo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
+import { ListarUsuarioComponent } from './pages/listar-usuario/listar-usuario.component';
 
 
 const routes: Routes = [
   {path:"nav",component: NavComponent },
+  {path:"usuario",component: ListarUsuarioComponent}
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
 import { AppRoutingModule } from './app-routing.module';
+import { CadastrarUsuarioComponent } from './pages/cadastrar-usuario/cadastrar-usuario.component';
+import { ListarUsuarioComponent } from './pages/listar-usuario/listar-usuario.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    CadastrarUsuarioComponent,
+    ListarUsuarioComponent
   ],
   imports: [
     BrowserModule,
